256664d9e2039e83103bc90c9542971603fb3601
Cameron Beccario <cambecc@nullschool.net>
2013-01-28 17:34:00 +0900

    simplify header files

--- src/main/c++/Rdrand.h
+++ src/main/c++/Rdrand.h
@@ -8,27 +8,17 @@
 
 #pragma once
 
+#include <stdint.h>
+#if defined(_MSC_VER)
+    #include <intrin.h>
+    #include <immintrin.h>
+#endif
+
 // Standardize terminology between different compilers.
 #if defined(_WIN64) || defined(_LP64)
     #define _X64
 #else
     #define _X86
-#endif
-
-#if defined(_MSC_VER)
-    #include <intrin.h>
-    #include <immintrin.h>
-//  #include <stdint.h>  // ?? does this work for vc?
-    typedef __int8 int8_t;
-    typedef __int16 int16_t;
-    typedef __int32 int32_t;
-    typedef __int64 int64_t;
-    typedef unsigned __int8 uint8_t;
-    typedef unsigned __int16 uint16_t;
-    typedef unsigned __int32 uint32_t;
-    typedef unsigned __int64 uint64_t;
-#elif defined(__GNUC__)
-    #include <stdint.h>
 #endif
 
 class Rdrand {
