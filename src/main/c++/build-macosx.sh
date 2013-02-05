52b53770346cc095574084ec178562ebf2c819b4
Cameron Beccario <cambecc@nullschool.net>
2013-02-05 12:43:32 +0900

    make newest refactoring build on macosx

--- src/main/c++/build-macosx.sh
+++ src/main/c++/build-macosx.sh
@@ -1,5 +1,5 @@
 # TODO: convert to make
 set -e
 set -x
-g++ -m64 -dynamiclib -fast -fPIC -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin *.cpp -o drnglib-x64.dylib
+gcc -Wall -std=c99 -m64 -dynamiclib -fast -fPIC -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin *.c -o drnglib-x64.dylib
 mv drnglib-x64.dylib ../resources/net/nullschool/util/macosx
