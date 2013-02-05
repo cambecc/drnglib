7aae859fbe386f2cfd47c24e707a36a6ee3defeb
Cameron Beccario <cambecc@nullschool.net>
2013-02-05 15:32:24 +0900

    make javah launcher a bit more sophisticated: normalize 64-bit literal suffix to C99

--- src/test/java/net/nullschool/util/JavahLauncher.java
+++ src/test/java/net/nullschool/util/JavahLauncher.java
@@ -1,16 +1,18 @@
 package net.nullschool.util;
 
-import java.io.PrintWriter;
+import java.io.*;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 /**
  * 2013-01-15<p/>
  *
- * A launcher for <a href="http://docs.oracle.com/javase/7/docs/technotes/tools/windows/javah.html">javah</a>
- * that forces a consistent line separator irrespective of the hosting platform. This ensures javah does not
- * replace an existing file simply due to differences in line separators, thus avoiding version control
- * noise.<p/>
+ * A launcher for <a href="http://docs.oracle.com/javase/7/docs/technotes/tools/windows/javah.html">
+ * javah</a> that normalizes the output to be consistent between platforms. Having platform-specific
+ * behavior causes version control noise when nothing has changed and is confusing.<p/>
  *
  * Released to the public domain: http://creativecommons.org/publicdomain/zero/1.0/
  *
@@ -18,20 +20,43 @@ import java.lang.reflect.Method;
  */
 public class JavahLauncher {
 
-    public static void main(String[] args) throws Exception {
-        String original = System.getProperty("line.separator");
-        try {
-            System.setProperty("line.separator", "\n");
+    /**
+     * Javah outputs different text depending on the operating system. For example,
+     * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5085132. Having platform-specific
+     * behavior causes version control noise when nothing has changed and is confusing. This
+     * method opens the specified file, normalizes the contents, then overwrites the original
+     * if modifications are required.
+     *
+     * @param file the file to normalize
+     * @throws IOException
+     */
+    private static void normalize(Path file) throws IOException {
+        String original = new String(Files.readAllBytes(file), "UTF-8");
+        String modified = original;
 
-            // Find and invoke com.sun.tools.javah.Main.run(String[], PrintWriter). Doing this dynamically
-            // means we can avoid polluting the project with a dependency on tools.jar.
-            Class<?> clazz = Class.forName("com.sun.tools.javah.Main");
-            Method method = clazz.getMethod("run", String[].class, PrintWriter.class);
-            method.invoke(null, args, new PrintWriter(System.out));
+        // Use C99 standard suffix "LL" instead of "i64" for 64-bit numeric literals.
+        modified = modified.replaceAll("(\\d)i64", "$1LL");
+        // Use UNIX-style line endings.
+        modified = modified.replaceAll("\\r\\n", "\n");
+
+        if (!modified.equals(original)) {
+            Files.write(file, modified.getBytes("UTF-8"));
         }
-        finally {
-            // Make sure to restore the original value since we may be running in a non-forked JVM.
-            System.setProperty("line.separator", original);
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Find and invoke com.sun.tools.javah.Main.run(String[], PrintWriter). Doing this dynamically
+        // means we can avoid polluting the project with a dependency on tools.jar.
+        Class<?> clazz = Class.forName("com.sun.tools.javah.Main");
+        Method method = clazz.getMethod("run", String[].class, PrintWriter.class);
+        method.invoke(null, args, new PrintWriter(System.out));
+
+        for (int i = 0; i < args.length; i++) {
+            // Find the output file argument:  -o <file>
+            if ("-o".equals(args[i]) && (i + 1 < args.length)) {
+                normalize(Paths.get(args[i + 1]));
+                break;
+            }
         }
     }
 }
